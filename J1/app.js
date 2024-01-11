8960bf37517d96abef471acf950f8e1c2d992be9
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-01-11 14:36:27 +0530

    Commit

---
+++ J1/app.js
@@ -0,0 +1,72 @@
+// console.log("Hello world!");
+
+// // var a;
+// // a = 10;
+// // console.log(a);
+
+// // let a;
+// // a = "Kuch bhi";
+// // console.log(a);
+
+// // const a;
+// // a = "Kuch bhi"; not possible
+
+// const a = "Kuch bhi";
+// console.log(a);
+
+
+// var a = 10;
+// var a = 32;
+// a = 32;
+// console.log(a);
+
+
+// let a = "String hai bhaiya"
+// let a = 26;
+// a = 26;
+// console.log(a);
+
+// const a = 36;
+// const a = "ABC;
+// a = 72;
+// console.log(a);
+
+// let a = 22.98;
+// let a = '22';
+// let a = true;
+// let a;
+// console.log(a);
+// console.log(typeof(a));
+
+
+let arr = [1, typeof("abc"),"String", false, undefined];
+// console.log(arr);
+// console.log(arr[1]);
+
+
+// let i = 0;
+
+// while(i < 5)
+// {
+//     console.log(arr[i]); ghatiya hai
+//     i++;
+// }
+
+// for(let i = 0; i < 5; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+for(let item of arr)
+{
+    console.log(item);
+}
+
+
+
+
+
+
+
+
+
