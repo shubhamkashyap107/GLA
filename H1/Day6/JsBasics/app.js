ac8a8ff599e8970c1aec034ffc9309b8deea0f16
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-12 11:30:15 +0530

    Commit

---
+++ H1/Day6/JsBasics/app.js
@@ -0,0 +1,39 @@
+// console.log("HELLO WORLD");
+
+
+// let 1abc = "ABC";
+// let _name = "SHAKTIMAAN";
+// let $name = "SHAKTIMAAN";
+// let return = "XYZ";
+// let break = "ABC";
+// let RETURN = "ABC";
+// let a b = "ABC";
+
+
+
+// let fn = "SHINCHAN"
+// let ln = "NOHARA"
+
+// console.log(fn + " " + ln);
+
+
+// let fn = 'shinchan'
+
+
+// let address = 'shubham's home'
+
+// let address = "shubham's home"
+// console.log(address);
+
+// let address = 'shubham\'s home'
+// console.log(address);
+
+// let fullname = 'shinchan \n nohara'
+// console.log(fullname);
+
+
+let fn = "Bhupendra"
+let ln = "Jogi"
+
+// console.log(`Hello ${fn} ${ln}, Welcome to my class!`);
+console.log(fn - ln)
---
+++ H1/Day6/Numbers/app.js
@@ -0,0 +1,20 @@
+// let a = 10;
+// console.log(a);
+
+// let max = Number.MAX_SAFE_INTEGER;
+// // let max = Number.MAX_VALUE
+// let min = Number.MIN_SAFE_INTEGER;
+
+// console.log(max);
+// console.log(min);
+
+
+// let hello = "Hello world aaj toh dhua uda denge";
+// console.log(hello[-1]);
+
+
+// let name = "DORILALBHAIYA"
+// console.log(name[100]);
+
+let name;
+console.log(name);
