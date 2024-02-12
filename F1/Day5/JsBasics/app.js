d225003be4050fceed624a6c736f05a2d44d7007
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-12 09:46:42 +0530

    Commit

---
+++ F1/Day5/JsBasics/app.js
@@ -0,0 +1,37 @@
+// console.log("HELLO WORLD");
+
+
+// let 1abc = "ABC";
+// let a b = "ABC"
+// let return = "ANC"
+// let RETURN = 1;
+// let _abc = "ABC";
+// let $abc = 123;
+
+
+// let firstName = "SHAKTI"
+// let lastName = "KAPOOR"
+
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(firstName + " " + lastName);
+
+
+
+let str = "ABC";
+let str2 = 'ABCDEF';
+// let address = 'Shubham's Home'
+// let address = "Shubham's Home"
+let address = 'Shubham\'s Home'
+console.log(address);
+
+let fullName = "SHUBHAM \n KASHYAP";
+console.log(fullName);
+
+
+let fn = "SHAKTI"
+let ln = "KAPOOR"
+
+console.log(`Hello ${fn} ${ln} welcome to JS CLASS`)
+// console.log("Hello ${fn} ${ln} welcome to JS CLASS") -> NOT POSSIBLE
