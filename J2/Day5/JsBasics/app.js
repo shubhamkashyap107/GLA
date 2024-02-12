0b4107c5c0a0a61df4310892b34ce50a0cd7ba84
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-12 13:23:45 +0530

    Commit

---
+++ J2/Day5/JsBasics/app.js
@@ -0,0 +1,30 @@
+// console.log("HELLO WORLD");
+// let 1abc = 1;
+// let a b = "abc";
+// let RETURN = 1;
+// let return = 1;
+
+
+// let fn = "SHINCHAN"
+// let ln = "NOHARA"
+
+// console.log(fn + " " + ln);
+
+
+// let address = 'Shubham\'s home'
+// console.log(address);
+// let fullname = "SHubham \n Kashyap"
+// console.log(fullname);
+
+
+// let str = `ABCDE`
+// console.log(str);
+
+let fn = "Tanuj"
+let ln = "Pathak"
+
+
+console.log(`Hello my name is ${fn} ${ln}`);
+
+
+
---
+++ J2/Day5/Numbers/app.js
@@ -0,0 +1,13 @@
+// // let max = Number.MAX_SAFE_INTEGER;
+// // let min = Number.MIN_SAFE_INTEGER;
+// // console.log(max + " , " + min);
+// let str = "my name is lakhan"
+// console.log(str[5]);
+
+
+let str = "CHOTA BHEEM";
+console.log(str[100]);
+
+
+let name;
+console.log(name);
