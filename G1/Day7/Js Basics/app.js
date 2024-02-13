653017c6b0839dd983d0a678017f905666820c79
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-13 09:34:22 +0530

    Commit

---
+++ G1/Day7/Js Basics/app.js
@@ -0,0 +1,59 @@
+// // console.log("HELLO AGAIN");
+
+
+// // let 1a = "ABC";
+// // let b1a = "ABC";
+// // let a b = "ten";
+// // let if = 10;
+// // let RETURN = 10;
+// // let _abc = "abc";
+// // let $a_bc = "abc";
+
+
+// // let fn = "GABBAR"
+// // let ln = "SINGH"
+// // let nn = fn + " " + ln // console.log(fn + ln)
+// // console.log(nn);
+
+
+// let eg1 = "EXAMPLE";
+// let eg2 = 'EXAMPLE';
+
+
+
+// // let address = 'Shubham's Home'
+// // let address = "Shubham's Home"
+// let address = 'Shubham\'s Home'
+// console.log(address);
+
+
+// let fn = "Shubham \n Kashyap";
+// console.log(fn);
+
+
+
+
+// let name = `Shubham`
+// console.log(name);
+
+
+// let fn = "Nobita"
+// let ln = "Nobi";
+
+
+// console.log(`Hello my name is ${fn} ${ln}`);
+// console.log("Hello my name is ${fn} ${ln}");
+
+
+// let str = "My name is lakhan!"
+// console.log(str[5]);
+// console.log(str[-5]);
+
+
+// let str = "maine khatti toffee khayi hai"
+// console.log(str[100]);
+
+let name;
+console.log(name);
+
+
---
+++ G1/Day7/Numbers/app.js
@@ -0,0 +1,9 @@
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(Number.NEGATIVE_INFINITY);
+
+
