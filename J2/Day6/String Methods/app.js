b580823c1f5840831cff20849afe978ebdbdeb7b
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-14 15:08:25 +0530

    Commit

---
+++ J2/Day6/String Methods/app.js
@@ -0,0 +1,54 @@
+// let str = "munni badnam hui, rahul tere liye";
+// console.log(str.substr(2))
+// console.log(str.substr(2,5))
+
+
+// let str = "humko bhi valentines day banana hai"
+// console.log(str.substring(2))
+// console.log(str.substring(2,6))
+
+
+// let str = "mai khatti toffee khaya hu";
+// console.log(str.indexOf("a", 2))
+
+
+// console.log(str.indexOf('a', str.indexOf('a',str.indexOf("a") + 1) + 1))
+
+
+
+
+// let abc = "mai hu mahesh d"
+// console.log(abc.replace("m", 'z'));
+// console.log(abc.replaceAll("m", 'z'));
+
+
+// let str = "bhau";
+// console.log(str.repeat(10))
+// console.log(str.toUpperCase());
+// let str2 = "BHAU"
+// console.log(str2.toLowerCase());
+
+
+// "              uhasdhdf              "
+// "              uhas        dhdf              "
+
+
+
+let msg = "           EXAM     PLE            "
+console.log(msg);
+console.log(msg.trim());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
