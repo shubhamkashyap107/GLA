aee2848201b30134a51067b57b7bd79bac9d9eb8
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-15 11:48:48 +0530

    Commit

---
+++ H1/Day7/General Vs Strict Equality/app.js
@@ -0,0 +1,47 @@
+// // let a = 10;
+// // let b = "10";
+
+// // console.log(a == b)
+// // console.log(a === b) //also checks the datatype
+// // console.log(a == a);
+// // console.log(1 == true);
+// // console.log(1 === true);
+// // console.log('1' == true);
+// // console.log(undefined == 0);
+// // console.log(" " == 0);
+// // console.log(null == 0);
+
+
+// // console.log(" " == 0);
+// // console.log(" " == false);
+// // console.log(0 == false);
+
+
+// // console.log([] == 0);
+// // console.log([] == " ");
+
+
+// // console.log(10 == 10.0);
+// // console.log(10 === 10.00);
+
+// let a = "example";
+// let b = 22;
+// // kuchbhi = 10
+// console.log(typeof(a));
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+// console.log(typeof(true));
+// console.log(typeof(kuchbhi));
+
+
+// let ans = 0 / 10;
+// let ans = 10 / 0;
+let ans = 0 / 0;
+
+
+console.log(ans);
+
+
+
+
+
---
+++ H1/Day7/String Methods/app.js
@@ -0,0 +1,34 @@
+// let str = "munni badnam hui bhaiya tere liye"
+// console.log(str.substr(2))
+// console.log(str.substr(2,5))
+
+
+// let str = "mai wo ek sapna hu jo sabke sapne sach mai kru"
+// console.log(str.substring(2))
+// console.log(str.substring(2,10))
+
+
+// let abc = "mai nikla gaddi leke"
+// console.log( abc.replace('a', "z") )
+// console.log( abc.replaceAll('a', "z") )
+
+// let str = "TOM "
+// console.log(str.repeat(10))
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let msg = "            EXAMPLE               "
+// let msg2 = "            EXAM          PLE2               "
+
+
+// console.log(msg.trim())
+// console.log(msg2.trim())
+// console.log(msg2.trimStart())
+// console.log(msg2.trimEnd())
+
+
+let str = "ye haath mujhe dede thakur"
+// console.log(str.indexOf('e'));
+console.log(str.indexOf('e', str.indexOf('e', str.indexOf('e') + 1 ) + 1));
+
+
