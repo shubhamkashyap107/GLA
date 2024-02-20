73a3ee4e21d28c81f8e9bf5d27cb870beeb513b1
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-20 13:51:53 +0530

    Commit

---
+++ J1/Day8/Arrays/app.js
@@ -0,0 +1,113 @@
+// let arr = [];
+// let arr = [1,'two', true, null,"ANSWER", undefined, [], function(){}, obj = {}]
+
+// console.log(arr[4]);
+// console.log(arr[-1]);
+// console.log(arr[100]);
+
+
+// let nestedArray = [1,2,3,4,5,['a','b','c',[true, undefined, "ANSWER"]]];
+// console.log(nestedArray[5][3][2]);
+
+
+// let arr2 = nestedArray[5]
+// console.log(arr2);
+// let arr3 = arr2[3]
+// console.log(arr3);
+// console.log(arr3[2]);
+
+
+
+
+// let arr = [1,2,3, true, undefined, "ABC"]
+// console.log(arr);
+// console.log(typeof(arr));
+
+
+
+// let arr = [1,2,3,4,5,6,7,89];
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.push(10)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// arr.unshift(-22)
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// let narr = arr.slice(0,3);
+// console.log(narr);
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// console.log(arr);
+// arr.splice(0,2);
+// console.log(arr);
+
+
+
+// let arr = ['a','d','c','d']
+// // console.log(arr.includes('de'));
+// console.log(arr.indexOf('e'));
+// console.log(arr.reverse());
+
+// let url = "https://www.google.com"
+// // let url = "a/b/c/d"
+
+// let ans = url.split('/')
+// console.log(ans);
+// console.log(ans.join("/"));
+
+// let arr1 = [1,2,3,4,5]
+// let arr2 = [10,20,30,40,50]
+
+// let ans = arr1.concat(arr2);
+// console.log(ans);
+
+// console.log(arr1.concat(arr2));
+// let ans = arr2.concat(arr1)
+// console.log(ans);
+
+
+let arr1 = [1,2,3,4];
+let arr2 = arr1;
+console.log(arr2);
+arr1.pop()
+console.log(arr2);
+arr2.pop()
+console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
