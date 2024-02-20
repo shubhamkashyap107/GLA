64866d6801a93ee5353c2fecb153416a85219d1d
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-02-20 15:33:11 +0530

    Commit

---
+++ B1/Day7/Arrays/app.js
@@ -0,0 +1,89 @@
+// let arr = [];
+// console.log(arr);
+
+
+
+// let arr = [1,"two", true, null, undefined,"ANSWER", [], obj = {}, function(){}]
+// console.log(arr);
+// console.log(arr[5]);
+
+
+// let nestedArray = [1,2,3,4,5,['a','b',null, true, [undefined, 123, "ANSWER"]]]
+// console.log(nestedArray[5][4][2]);
+
+// let arr = nestedArray[5];
+// console.log(arr);
+// let arr2 = arr[4]
+// console.log(arr2);
+// console.log(arr2[2]);
+
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// arr.pop();
+// console.log(arr);
+// arr.push(10)
+// console.log(arr);
+// // arr.pop(0)
+// arr.shift();
+// console.log(arr);
+// arr.unshift(22)
+// console.log(arr);
+
+// let arr = [1,2,3,4,5];
+// let narr = arr.slice(0,2);
+// console.log(narr);
+
+
+// arr.splice(0,2);
+// console.log(arr);
+
+// let arr = ['a','b','c','d'];
+// let arr2 = [1,2,3,4,5];
+
+
+
+// console.log(arr.includes("ce"));
+// console.log(arr.indexOf('ce'));
+
+// let narr = arr.concat(arr2);
+// console.log(narr);
+
+
+// let url = "https://www.google.com"
+// let ans = url.split("/")
+// console.log(ans);
+// console.log(ans.join("/"))
+
+
+let arr = [1,2,3,4,5]
+// console.log(arr.reverse());
+// console.log(arr);
+let arr2 = arr;
+console.log(arr2);
+arr.pop()
+console.log(arr2);
+arr2.push(100)
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ B1/Day7/Math/app.js
@@ -0,0 +1,43 @@
+// console.log(Math.PI);
+
+
+// let a = 10.50;
+// console.log(Math.round(a));
+// console.log(Math.ceil(a));
+// console.log(Math.floor(a));
+
+// let a = 10;
+// let b = 22;
+// console.log(Math.min(a,b));
+// console.log(Math.max(a,b));
+
+// let a = 49;
+// console.log(Math.sqrt(a));
+
+
+// let random = Math.random();
+// console.log(random);
+
+
+
+// let coin = Math.ceil(Math.random() * 2)
+// console.log(coin);
+
+// if(coin == 1)
+// {
+//     console.log("HEAD");
+// }
+// else
+// {
+//     console.log("TAIL");
+// }
+
+// let dice = Math.ceil(Math.random() * 6)
+// console.log(dice);
+
+
+
+
+
+
+
