d167255b39e432516fbecdf025f64e5347aa72e7
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-03-12 15:42:32 +0530

    Commit

---
+++ B1/Day9/Async Programming/app.js
@@ -0,0 +1,11 @@
+
+setTimeout( () => {
+    console.log("Chutney LAAAAAAA!!!");
+} ,5000 )
+console.log("Pakode bana");
+for(let i = 0; i < 999999999999; i++)
+{
+
+}
+// console.log("Chutney laaaa")
+console.log("Pakode kha")
---
+++ B1/Day9/Functions/app.js
@@ -0,0 +1,98 @@
+// function fun(){
+//     // console.log("HAAN V");
+//     // return undefined
+
+//     function inner(){
+//         console.log("Hello from inner");
+//     }
+//     // inner()
+
+//     return inner
+
+// }
+
+// // fun()
+// // console.log(fun());
+// let retVal = fun()
+// console.log(retVal);
+// retVal()
+// // inner()
+
+
+
+// let func = function(){
+//     console.log("Hello");
+// }
+
+// console.log(func);
+// abc()
+// func()
+
+
+
+
+
+// let func = () => {
+//     console.log("Hello");
+// }
+// func()
+
+
+// function sum(a, b){
+//     console.log(a + b);
+// }
+
+// sum(1,2)
+
+// function calculator(a,b, fn){
+//     fn(a,b)
+// }
+
+// function sum(a, b)
+// {
+//     console.log(a + b);
+// }
+
+// function mul(a, b)
+// {
+//     console.log(a * b);
+// }
+
+
+// calculator(3,4, sum)
+
+
+
+let arr = [2,4,3,1,9,0];
+
+let ans = arr.filter( (i) => {return i % 2 == 0} )
+// let ans = arr.filter(
+//     // function(item){
+//     //     return item % 2 == 0
+//     // }
+
+
+// )
+
+
+console.log(ans);
+
+
+
+// for(let i = 0; i < arr.length ; i++)
+// {
+//     if(arr[i] % 2 == 0)
+//     {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
