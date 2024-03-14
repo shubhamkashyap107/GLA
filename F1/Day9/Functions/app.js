576a779f59c4a4c1320842cdc20efe99a643486d
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-03-14 09:48:40 +0530

    COmmit

---
+++ F1/Day9/Functions/app.js
@@ -0,0 +1,44 @@
+// function abc()
+// {
+//     console.log("Hello from abc function");
+    
+
+//     function xyz(){
+//         console.log("Hello from xyz");
+//         return "Returned from xyz fn"
+//     }
+
+
+//     return xyz
+// }
+// let innerFN = abc()
+// console.log(innerFN());
+
+
+// abc()
+// console.log(abc())
+// xyz()
+
+
+// function abc(){
+    
+// }
+
+// let fn = function(){
+//     console.log("HELLO");
+// }
+
+// abc()
+// fn()
+
+
+
+
+// let af = () => {
+//     console.log("MEOW");
+// }
+
+// af()
+
+
+
---
+++ F1/Day9/Loops/app.js
@@ -0,0 +1,78 @@
+// let i = 1;
+// while(i <= 10)
+// {
+//     // console.log(i)
+//     console.log("PAKODE");
+//     i++;
+// }
+
+
+// let i = 10000;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i <= 10)
+
+
+
+// let a = 10
+
+// for(  ; a < 100; a+=10)
+// {
+//     console.log(a);
+// }
+
+
+
+
+// let arr = ["a", "b", "c", "d", "e"]
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+
+// let i = 0;
+// while(i <= 4)
+// {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+
+// for(let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+
+// let arr = ["a", "b", "c", "d", "e"]
+
+// for(let sk of arr)
+// {
+//     console.log(sk);
+// }
+
+
+
+
+
+let obj = {
+    name : "bagga",
+    age : 24,
+    favCol : "Black"
+}
+// console.log(obj["age"])
+// console.log(obj.age);
+
+for(let item in obj){
+    console.log(obj[item]);
+}
+
+
+
+
+
+
