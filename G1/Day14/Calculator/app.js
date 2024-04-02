dec1c5ca544cd9f1d170d8d2010a95d60c35328b
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-04-02 09:32:53 +0530

    Commit

---
+++ G1/Day14/Calculator/app.js
@@ -0,0 +1,34 @@
+let disp = document.getElementById("display")
+let btns = document.getElementsByTagName("button")
+
+
+for(let item of btns)
+{
+    item.addEventListener("click", function(event){
+        if(event.target.innerText == "AC")
+        {
+            disp.value = 0
+        }
+        else if(event.target.innerText == "=")
+        {
+            try {
+                let ans = eval(disp.value)
+                disp.value = ans        
+            } catch (error) {
+                -
+            }
+        }
+        else
+        {
+            disp.value += event.target.innerText
+        }
+    })
+}
+
+
+
+
+
+
+
+
